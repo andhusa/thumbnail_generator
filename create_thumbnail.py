a07119fd717df2fd29cb58984129e7f809d0b517
Andreas Husa <husaandreas@gmail.com>
2021-12-17 11:00:14 +0100

    added face detection flags

--- create_thumbnail.py
+++ create_thumbnail.py
@@ -1,49 +1,75 @@
 import cv2
 import os
 import re
+import sys
 from moviepy.editor import *
 from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
-#import numpy as np
-#import imutils
+
 from os.path import isfile, join
 import shutil
 import imquality.brisque as brisque
 import PIL.Image
 import dlib
-#from mtcnn.mtcnn import MTCNN
 
-#folder_path = "/global/D1/projects/soccer_clipping/events-Allsvenskan2019-minus15-pluss25/"
-folder_path = "/global/D1/projects/soccer_clipping/events-Eliteserien2019-minus15-pluss25/"
+folder_path = "/global/D1/projects/soccer_clipping/events-Allsvenskan2019-minus15-pluss25/"
+#folder_path = "/global/D1/projects/soccer_clipping/events-Eliteserien2019-minus15-pluss25/"
 #folder_path = "/home/andrehus/egne_prosjekter/videoAndOutput/"
 #model = keras.models.load_model('/home/andrehus/egne_prosjekter/videoAndOutput/models/thumbnail_vs_no_thumbnail.h5')
 #model = keras.models.load_model('/home/andrehus/egne_prosjekter/videoAndOutput/models/thumbnail_vs_no_thumbnail_v2_model.h5')
-model = keras.models.load_model('/home/andrehus/egne_prosjekter/videoAndOutput/models/close_up_model.h5')
+close_up_model = keras.models.load_model('/home/andrehus/egne_prosjekter/videoAndOutput/models/close_up_model.h5')
 logo_detection_model = keras.models.load_model('/home/andrehus/egne_prosjekter/videoAndOutput/models/logo_detection/logo_detection.h5')
-thumbnail_output = folder_path + "/thumbnail_output/"
-num_videos = 1
-
-logoTesting = "/home/andrehus/egne_prosjekter/videoAndOutput/AllsvenskanTestLogo/Background"
-#logoTesting = "/home/andrehus/egne_prosjekter/videoAndOutput/logos"
+thumbnail_output = os.path.dirname(os.path.abspath(__file__)) + "/thumbnail_output/"
+num_videos = 5
+haarStr = "haar"
+dlibStr = "dlib"
 
 def main():
+    faceDetModel = ""
+    try:
+        argument = sys.argv[1]
+        if argument == "-" + haarStr:
+            faceDetModel = haarStr
+            print("Using Haar face detection model")
+        elif argument == "-" + dlibStr:
+            faceDetModel = dlibStr
+            print("Using Dlib face detection model")
+        else:
+            raise
+    except:
+        print("")
+        print("Specify with flag:")
+        print("")
+        print("Faster generating, but more unpresice:")
+        print("python " + os.path.basename(__file__) + " -haar")
+        print("")
+        print("Slower generating, but more presice:")
+        print("python " + os.path.basename(__file__) + " -dlib")
+        return
+    
     try:
         if not os.path.exists(thumbnail_output):
             os.makedirs(thumbnail_output)
+
+            for f in os.listdir(thumbnail_output):
+                print(f)
+                #os.remove(os.path.join(thumbnail_output, f))
     except OSError:
         print("Error: Couldnt create thumbnail_output directory")
+        return
     i = 0
     for f in os.listdir(folder_path):
         if i >= num_videos:
             return
         name, ext = os.path.splitext(f)
         if ext == ".ts":
-            create_thumbnail(name + ext)
+            create_thumbnail(name + ext, faceDetModel)
             i += 1
         
 
-def create_thumbnail(video_filename):
-    print("PREDICTING")
+def create_thumbnail(video_filename, faceDetModel):
+    print("Finding thumbnail for: ") 
+    print(video_filename)
     video_path = folder_path + video_filename
     frames_folder = folder_path + video_filename.split(".")[0] + "_frames"
     # Read the video from specified path
@@ -93,67 +119,66 @@ def create_thumbnail(video_filename):
     # Release all space and windows once done
     cam.release()
     cv2.destroyAllWindows()
-    priority_images = predictAndRemove(frames_folder)
+    priority_images = groupFrames(frames_folder, faceDetModel)
+    finalThumbnail = ""
     for priority in priority_images:
+        if finalThumbnail != "":
+            break
         priority = dict(sorted(priority.items(), key=lambda item: item[1], reverse=True))
-        print(priority)
-
-        highestPri = 0
-        image = ""
-        scores = []
-        i = 0
+        print("")
+        print("Frames in group: ")
         for key in priority:
-            scores.append(predictBeauty(key))
-            if scores[i] < 35:
-                highestPri = priority[key]
+            print(key.split("/")[-1])
+        bestScore = 0
+        for key in priority:
+            score = predictBrisque(key)
+            if finalThumbnail == "":
+                bestScore = score
+                finalThumbnail = key
+            if score < 35:
                 image = key
                 break
-            i += 1
+            if score < bestScore:
+                bestScore = score
+                finalThumbnail = key
 
-        #If there are none images above the threshold score    
-        if image == "" and len(scores) > 0:
-            min_score = min(scores)
-            min_index = scores.index(min_score)
-            i = 0
-            for key in priority:
-                if min_index == i:
-                    image = key
-                    break
-                i += 1
         
-        if image != "":
-            newName = video_filename.split(".")[0] + "_thumbnail.jpg"
-            imageName = image.split("/")[-1].split(".")[0]
-            frameNum = int(imageName.replace("frame", ""))
-            cam = cv2.VideoCapture(video_path)
-            curFrame = 0
-            while(True):
-                ret, frame = cam.read()
-                if curFrame == frameNum:
-                    cv2.imwrite(thumbnail_output + newName, frame)
-                    break
-                curFrame += 1
+    if finalThumbnail != "":
+        newName = video_filename.split(".")[0] + "_thumbnail.jpg"
+        imageName = finalThumbnail.split("/")[-1].split(".")[0]
+        frameNum = int(imageName.replace("frame", ""))
+        cam = cv2.VideoCapture(video_path)
+        curFrame = 0
+        while(True):
+            ret, frame = cam.read()
+            if curFrame == frameNum:
+                cv2.imwrite(thumbnail_output + newName, frame)
+                break
+            curFrame += 1
 
-            cam.release()
-            cv2.destroyAllWindows()
-            try:
-                shutil.rmtree(frames_folder)
-            except OSError as e:
-                print("Error: %s - %s." % (e.filename, e.strerror))
-            return
+        cam.release()
+        cv2.destroyAllWindows()
+        print("")
+        print("Final thumbnail frame number: " + str(frameNum))
+        try:
+            shutil.rmtree(frames_folder)
+        except OSError as e:
+            print("Error: %s - %s." % (e.filename, e.strerror))
+        return
+    return
 
 
 
-
-def predictBeauty(image_path):
+def predictBrisque(image_path):
     img = PIL.Image.open(image_path)
-    beautyScore = brisque.score(img)
-    print(image_path)
-    print("Beauty score:")
-    print(beautyScore)
-    return beautyScore
+    brisqueScore = brisque.score(img)
+    print("")
+    print(image_path.split("/")[-1])
+    print("Brisque score:")
+    print(brisqueScore)
+    return brisqueScore
 
-def predictAndRemove(frames_folder):
+def groupFrames(frames_folder, faceDetModel):
     
     #frames_folder is your directory path as string
     test_data_generator = ImageDataGenerator(rescale=1./255)
@@ -161,30 +186,6 @@ def predictAndRemove(frames_folder):
     TEST_SIZE = len(next(os.walk(frames_folder + "/frames"))[2]) 
     print("TEST SIZE: " + str(TEST_SIZE))
     IMAGE_WIDTH, IMAGE_HEIGHT = IMAGE_SIZE, IMAGE_SIZE
-    '''
-    LOGO_TEST_SIZE = len(next(os.walk(logoTesting + "/frames"))[2])
-    print("TEST SIZE: " + str(LOGO_TEST_SIZE))
-
-    test_generator_logo = test_data_generator.flow_from_directory(
-        logoTesting,
-        target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-        batch_size=1,
-        class_mode="binary",
-        shuffle=False)
-    logo_test_prob = logo_detection_model.predict_generator(test_generator_logo, LOGO_TEST_SIZE)
-    for index, probability in enumerate(logo_test_prob):
-        if probability > 0.05:
-            
-            print("LOGO")
-            print(probability)
-            print(test_generator_logo.filenames[index])
-        else:
-            print("NOT LOGO")
-            print(probability)
-            print(test_generator_logo.filenames[index])
-    '''
-
-
 
     test_generator = test_data_generator.flow_from_directory(
         frames_folder,
@@ -198,95 +199,65 @@ def predictAndRemove(frames_folder):
     for index, probability in enumerate(logo_probabilities):
         image_path = frames_folder + "/" + test_generator.filenames[index]
         if probability > 0.1:
-            print(image_path)
-            print("LOGO")
             logos.append(image_path) 
 
-    probabilities = model.predict_generator(test_generator, TEST_SIZE)
-    priority_images = [{} for x in range(5)]
+    probabilities = close_up_model.predict_generator(test_generator, TEST_SIZE)
+    priority_images = [{} for x in range(4)]
 
     for index, probability in enumerate(probabilities):
-
+        print("")
+        print(test_generator.filenames[index])
+        print("close-up prediction score: " + str(probability))
         image_path = frames_folder + "/" + test_generator.filenames[index]
-        #Beauty score 
-        #print("")
-        #print(""))
         if image_path in logos:
+            print("Logo detected")
             priority_images[3][image_path] = probability
-            continue
-        print(image_path)
-        print("probability:" + str(probability))
-        if probability > 0.6:
-            print("DETECT")
-            #print(image_path)
-            #print("Probability: " + str(probability[0]*100) + " thumbnail")
-            #img = PIL.Image.open(image_path)
-            #beautyScore = brisque.score(img)
-            #print(image_path)
-            #print(beautyScore)
-            #print("Big face detected: " + str(detect_faces(image_path)))
-            if not detect_faces(image_path):
-                #os.remove(image_path)
-                priority_images[1][image_path] = probability
-            else:
+        elif probability > 0.6:
+            face_size = detect_faces(image_path, faceDetModel)
+            if face_size > 0:
+                print("Face detected: " + str(face_size) + "px")
                 priority_images[0][image_path] = probability
+            else:
+                priority_images[1][image_path] = probability
         else:
-            #print(image_path)
-            #print("Probability: " + str((1-probability[0])*100) + " no-thumbnail")
             priority_images[2][image_path] = probability
             
     return priority_images
 
 
 
-def detect_faces(image):
-    
-    detector = dlib.get_frontal_face_detector()
-    img = cv2.imread(image)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = detector(gray, 1) # result
-    #to draw faces on image
-    for result in faces:
-        x = result.left()
-        y = result.top()
-        x1 = result.right()
-        y1 = result.bottom()
-        cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
-        cv2.imwrite(image, img)
-    if len(faces) > 0:
-        return True
-    return False
-    '''
-    detector = MTCNN()
-    img = cv2.imread(image)
-    faces = detector.detect_faces(img)# result
-    #to draw faces on image
-    for result in faces:
-        x, y, w, h = result['box']
-        x1, y1 = x + w, y + h
-        cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
-        cv2.imwrite(image, img)
-        return True
-    return False
-    
-    
-    # Load the cascade
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    # Read the input image
-    img = cv2.imread(image)
-    # Convert into grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+def detect_faces(image, faceDetModel):
+    biggestFace = 0
 
-    for (x, y, w, h) in faces:
-        #Want the size of face to be bigger than 30 pixels in at least one dimension
-        if w > 35 or h > 35:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.imwrite(image, img)
-            return True
+    if faceDetModel == dlibStr:
 
-    return False
-    '''
+        detector = dlib.get_frontal_face_detector()
+        img = cv2.imread(image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = detector(gray, 1)
+        for result in faces:
+            x = result.left()
+            y = result.top()
+            x1 = result.right()
+            y1 = result.bottom()
+            size = y1-y
+            if biggestFace < size:
+                biggestFace = size
+
+    elif faceDetModel == haarStr:
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        img = cv2.imread(image)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        biggestFace = 0
+        for (x, y, w, h) in faces:
+            if biggestFace < h:
+                biggestFace = h
+
+    else:
+        print("No face detection model in use")
+
+    return biggestFace
+
 if __name__ == "__main__":
     main()
